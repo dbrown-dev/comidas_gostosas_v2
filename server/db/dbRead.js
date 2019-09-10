1a3c2b7a957670f04ccb0462e38178171891f52a
David Brown <david.brown@windowslive.com>
2019-09-11 10:31:47 +1200

    updated recipes query

--- server/db/dbRead.js
+++ server/db/dbRead.js
@@ -9,6 +9,62 @@ const database = knex(config);
 const selectTable = curry(async (db, tableName) => db(tableName));
 const selectTableProd = selectTable(database);
 
+// Joins to select all summary recipe data for one or more recipes
+const getRecipeSummaries = (id, db = database) => {
+  return db('recipes')
+    .join('cook_times', 'recipes.cook_time_id', 'cook_times.id')
+    .join('seasons', 'recipes.season_id', 'seasons.id')
+    .join('cuisine_categories_recipes', 'recipes.id', 'cuisine_categories_recipes.recipe_id')
+    .join('cuisine_categories', 'cuisine_categories.id', 'cuisine_categories_recipes.category_id')
+    .select(
+      'recipes.id',
+      'recipes.title',
+      'seasons.label as season',
+      'recipes.rating',
+      'recipes.image',
+      'cook_times.label as cookTime',
+      db.raw('GROUP_CONCAT(cuisine_categories.label, "@") cuisine_categories')
+    )
+    .orderBy('recipes.id')
+    .groupBy('recipes.id')
+    .modify(queryBuilder => {
+      if (id) {
+        queryBuilder.where('recipes.id', id);
+        queryBuilder.first();
+      }
+    });
+};
+
+// Joins to select all instructions for a single recipe
+const getRecipeInstructions = (id, db = database) => {
+  return db('instructions')
+    .join('recipes', 'recipes.id', 'instructions.recipe_id')
+    .select('instructions.id', 'instructions.instruction', 'instructions.image')
+    .orderBy('instructions.id')
+    .where('recipes.id', id)
+    .groupBy('instructions.id');
+};
+
+// Joins to select all ingredients for a single recipe
+const getRecipeIngredients = (id, db = database) => {
+  return db('ingredients')
+    .join('ingredients_recipes', 'ingredients_recipes.ingredient_id', 'ingredients.id')
+    .join('recipes', 'ingredients_recipes.recipe_id', 'recipes.id')
+    .join('measurements', 'measurements.id', 'ingredients_recipes.measurement_id')
+    .select(
+      'ingredients.id',
+      'ingredients.label as ingredient',
+      'ingredients_recipes.quantity',
+      'ingredients_recipes.ingredient_group as ingredientGroup',
+      'measurements.label as measurement'
+    )
+    .orderBy('ingredients.id')
+    .where('recipes.id', id);
+};
+
 module.exports = {
-  selectTableProd
+  selectTableProd,
+  getRecipeSummaries,
+  getRecipeInstructions,
+  getRecipeIngredients
 };
